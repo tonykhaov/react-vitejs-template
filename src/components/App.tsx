3b84242e3bb841e2eeafb873eb50fa02b11e4d3e
Tony Khaov <tony.khaov@gmail.com>
2021-03-15 18:21:23 +0100

    update tailwind to use jit
    update packages especially tailwind/jit important classes utility

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import Query from './Query'
 
 function App() {
   return (
-    <div className="border border-black">
+    <div className="border !border-black">
       <h1 className="text-2xl text-center uppercase">React lab</h1>
 
       <Query />
