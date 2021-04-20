936643d15b747548813b588e3f76f5fa112f13f7
Tony Khaov <tony.khaov@gmail.com>
2021-04-20 15:42:03 +0200

    chore: update packages

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,15 +1,3 @@
 import { rest } from 'msw'
-import { buildPost } from './generate'
-import type { Post } from '@src/types/post'
 
-export const handlers = [
-  rest.get<undefined, Post[]>('/posts', (req, res, ctx) => {
-    return res(
-      ctx.json(
-        Array(5)
-          .fill(null)
-          .map(() => buildPost()),
-      ),
-    )
-  }),
-]
+export const handlers = []
