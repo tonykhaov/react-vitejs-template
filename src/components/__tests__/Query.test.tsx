e4e64275b44a33585f1c4e5db837546d3f9d3e46
Tony Khaov <tony.khaov@gmail.com>
2021-05-24 12:54:59 +0200

    chore: remove Query.tsx and react-query setup in jest

--- src/components/__tests__/Query.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen, waitForElementToBeRemoved } from '@src/utils/test/app-test-utils'
-import Query from '@src/components/Query'
-
-test('renders Query with a loading text first and then the content', async () => {
-  render(<Query />)
-  await screen.findByText(/e/gi)
-})
