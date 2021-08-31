90ecb348c12086eeb25e433f5551a935d1c64d85
Tony Khaov <tony.khaov@gmail.com>
2021-08-31 13:46:31 +0200

    chore: add react-query devtools

--- src/context/app-providers.tsx
+++ src/context/app-providers.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
 const AppProviders: React.FC = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
