0f627f66c6c33a6b7391485f130ba8cbd1d9cf13
Tony Khaov <tony.khaov@gmail.com>
2022-03-13 23:37:37 +0100

    feat: avoid refetching if data is in cache

--- src/pages/fetch.tsx
+++ src/pages/fetch.tsx
@@ -1,11 +1,14 @@
 import Title from '@src/components/Title'
 import type { User } from '@src/types/user'
-import { useQuery } from 'react-query'
+import { useQuery, useQueryClient } from 'react-query'
 
 function Fetch() {
-  const { data, status } = useQuery<User[]>({
+  const queryClient = useQueryClient()
+
+  const { data, status, refetch } = useQuery<User[]>({
     queryKey: 'users',
     queryFn: () => fetch('https://my-backend.com/api/users').then((res) => res.json()),
+    enabled: !queryClient.getQueryData('users'),
   })
 
   return (
@@ -14,15 +17,20 @@ function Fetch() {
       <div className="my-4 min-w-[50vw]">
         {status === 'loading' ? <p>Loading...</p> : null}
         {status === 'success' ? (
-          <ul>
-            {data.map((user) => (
-              <li className="max-w-5 my-2 w-full border p-4" key={user.id}>
-                <Title className="text-xl">{user.name}</Title>
-                <p>{user.email}</p>
-                <p>{user.password}</p>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <button className="rounded-lg bg-gray-300 px-8 py-4" onClick={() => refetch()}>
+              Refetch!
+            </button>
+            <ul>
+              {data.map((user) => (
+                <li className="my-2 w-full border p-4" key={user.id}>
+                  <Title className="text-xl">{user.name}</Title>
+                  <p>{user.email}</p>
+                  <p>{user.password}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         ) : null}
       </div>
     </>
