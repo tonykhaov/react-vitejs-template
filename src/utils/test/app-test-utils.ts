d6484064222c909f762e4558e4b63bd0074cc405
Tony Khaov <tony.khaov@gmail.com>
2022-05-10 00:04:39 +0200

    test: allow dev to pass userEvent options to render

--- src/utils/test/app-test-utils.ts
+++ src/utils/test/app-test-utils.ts
@@ -2,20 +2,24 @@ import { render as rtlRender } from '@testing-library/react'
 import type { RenderOptions, RenderResult } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import type { UserEvent } from '@testing-library/user-event/dist/types/setup'
+import type { Options as UserEventOptions } from '@testing-library/user-event/dist/types/options'
+
 import AppProviders from '@src/context/app-providers'
+
+type CustomRenderOptions = {
+  render: Omit<RenderOptions, 'queries'>
+  userEvent: UserEventOptions
+}
 
 type CustomRenderResult = {
   user: UserEvent
 } & RenderResult
 
-function customRender(
-  ui: React.ReactElement,
-  options?: Omit<RenderOptions, 'queries'>
-): CustomRenderResult {
-  const renderResult = rtlRender(ui, { wrapper: AppProviders, ...options })
+function customRender(ui: React.ReactElement, options?: CustomRenderOptions): CustomRenderResult {
+  const renderResult = rtlRender(ui, { wrapper: AppProviders, ...options?.render })
   return {
     ...renderResult,
-    user: userEvent.setup(),
+    user: userEvent.setup(options?.userEvent),
   }
 }
 
