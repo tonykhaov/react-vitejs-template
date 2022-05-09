b580d7372732c12af5ee069ec0a67511e69d54a7
Tony Khaov <tony.khaov@gmail.com>
2022-05-09 22:19:54 +0200

    test: use testing-library/user-event v14

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,23 +1,21 @@
-import { render, screen, userEvent, act } from '@utils/test/app-test-utils'
+import { render, screen } from '@utils/test/app-test-utils'
 import App from '@src/App'
 
-test('should render App with Homepage first and navigate through pages', () => {
-  // @testing-library/user-event v14 doesn't trigger a rerender.
-  // eventually fix this issue and remove the act() calls.
+test('should render App with Homepage first and navigate through pages', async () => {
+  const { user } = render(<App />)
 
-  render(<App />)
   // start with Homepage
   expect(screen.getByRole('heading', { name: /Homepage/i })).toBeInTheDocument()
 
   // navigate to About page
-  act(() => userEvent.click(screen.getByRole('link', { name: /go to about/i })))
-  expect(screen.getByRole('heading', { name: /About/i })).toBeInTheDocument()
+  user.click(screen.getByRole('link', { name: /go to about/i }))
+  expect(await screen.findByRole('heading', { name: /About/i })).toBeInTheDocument()
 
   // navigate to Fetch page
-  act(() => userEvent.click(screen.getByRole('link', { name: /go to fetch/i })))
-  expect(screen.getByRole('heading', { name: /fetch/i })).toBeInTheDocument()
+  user.click(screen.getByRole('link', { name: /go to fetch/i }))
+  expect(await screen.findByRole('heading', { name: /fetch/i })).toBeInTheDocument()
 
   // navigate to Homepage page
-  act(() => userEvent.click(screen.getByRole('link', { name: /go to homepage/i })))
-  expect(screen.getByRole('heading', { name: /Homepage/i })).toBeInTheDocument()
+  user.click(screen.getByRole('link', { name: /go to homepage/i }))
+  expect(await screen.findByRole('heading', { name: /Homepage/i })).toBeInTheDocument()
 })
--- src/utils/test/app-test-utils.ts
+++ src/utils/test/app-test-utils.ts
@@ -1,11 +1,23 @@
 import { render as rtlRender } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import type { RenderOptions, RenderResult } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import type { UserEvent } from '@testing-library/user-event/dist/types/setup'
 import AppProviders from '@src/context/app-providers'
 
-function render(ui: React.ReactElement, options?: Omit<RenderOptions, 'queries'>): RenderResult {
-  return rtlRender(ui, { wrapper: AppProviders, ...options })
+type CustomRenderResult = {
+  user: UserEvent
+} & RenderResult
+
+function customRender(
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+): CustomRenderResult {
+  const renderResult = rtlRender(ui, { wrapper: AppProviders, ...options })
+  return {
+    ...renderResult,
+    user: userEvent.setup(),
+  }
 }
 
 export * from '@testing-library/react'
-export { render, userEvent }
+export { customRender as render }
